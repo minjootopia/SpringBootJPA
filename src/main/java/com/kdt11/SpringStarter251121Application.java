baa1675d9e7e19944d2714325d9a4b920702c29f
minjoo <minjoook99@gmail.com>
2025-11-24 14:59:12 +0900

    spring boot first commit

---
+++ src/main/java/com/kdt11/SpringStarter251121Application.java
@@ -0,0 +1,13 @@
+package com.kdt11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarter251121Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarter251121Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kdt11/SpringStarter251121ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarter251121ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
